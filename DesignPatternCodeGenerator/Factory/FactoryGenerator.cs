b7359510d8f90954db48fdecd5b0c4a323df7976
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2022-10-10 21:26:52 +0200

    Adding references to projects. Adding FactoryGenerator class.

---
+++ DesignPatternCodeGenerator/Factory/FactoryGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternCodeGenerator.Factory
+{
+    [Generator]
+    public class FactoryGenerator
+    {
+
+    }
+}
