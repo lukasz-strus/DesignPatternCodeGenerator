145ff49ce1ac1dab0601c2d6e6f9d5a56c2fa61c
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2022-11-14 19:24:36 +0100

    Add Singleton Generator

---
+++ DesignPatternCodeGenerator/Singleton/SingletonGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternCodeGenerator.Singleton
+{
+    [Generator]
+    public class SingletonGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
