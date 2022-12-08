1f6817f4828a4f88ca7221104628408c9e394567
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2022-12-08 19:18:54 +0100

    Refactoring - Null Object

--- DesignPatternCodeGenerator/NullObject/NullObjectGenerator.cs
+++ DesignPatternCodeGenerator/NullObject/NullObjectGenerator.cs
@@ -2,8 +2,9 @@
 using DesignPatternCodeGenerator.Base.Enums;
 using DesignPatternCodeGenerator.Base.Generators;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
-using System.Diagnostics;
+using System.Linq;
 using System.Text;
 
 namespace DesignPatternCodeGenerator.NullObject
@@ -22,13 +23,16 @@ namespace DesignPatternCodeGenerator.NullObject
 
             foreach (var group in interfaceGroups)
             {
-                var classContent = NullObjectContentGenerator.GenerateClass(group);
-
-                context.AddSource(
-                $"{BaseNamesGenerator.GetClassName(group, GeneratorAttributeType.NullObject, true, true)}.g.cs",
-                SourceText.From(classContent, Encoding.UTF8));
+                GenerateNullObject(context, group);
             }
+        }
 
+        private static void GenerateNullObject(GeneratorExecutionContext context, IGrouping<string, InterfaceDeclarationSyntax> group)
+        {
+            var hintName = $"{BaseNamesGenerator.GetClassName(group, GeneratorAttributeType.NullObject)}.g.cs";
+            var classContent = NullObjectContentGenerator.GenerateClass(group);
+
+            context.AddSource(hintName, SourceText.From(classContent, Encoding.UTF8));
         }
 
         public void Initialize(GeneratorInitializationContext context)
