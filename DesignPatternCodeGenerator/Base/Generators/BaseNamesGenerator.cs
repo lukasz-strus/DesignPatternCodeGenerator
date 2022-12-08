4c23800971511a22a90a22cbef8ea5ba98c0684d
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2022-12-08 19:46:08 +0100

    Refactoring

--- DesignPatternCodeGenerator/Base/Generators/BaseNamesGenerator.cs
+++ DesignPatternCodeGenerator/Base/Generators/BaseNamesGenerator.cs
@@ -9,46 +9,24 @@ namespace DesignPatternCodeGenerator.Base.Generators
     internal static class BaseNamesGenerator
     {
         internal static string GetAccesibility(IGrouping<string, MemberDeclarationSyntax> group)
-            => group.First().FirstAncestorOrSelf<MemberDeclarationSyntax>().Modifiers.First().Text;
+            => group.First().Modifiers.First().Text;
 
         internal static string GetAccesibility(MemberDeclarationSyntax memberDeclarationSyntax)
-            => memberDeclarationSyntax.FirstAncestorOrSelf<MemberDeclarationSyntax>().Modifiers.First().Text;
+            => memberDeclarationSyntax.Modifiers.First().Text;
 
-        internal static string GetNamespace(IGrouping<string, TypeDeclarationSyntax> group)
-            => group.First().FirstAncestorOrSelf<NamespaceDeclarationSyntax>()?.Name?.ToString() ??
-               group.First().FirstAncestorOrSelf<FileScopedNamespaceDeclarationSyntax>().Name.ToString();
-
-        internal static string GetNamespace(IGrouping<string, MethodDeclarationSyntax> group)
-            => group.First().FirstAncestorOrSelf<NamespaceDeclarationSyntax>()?.Name?.ToString() ??
-               group.First().FirstAncestorOrSelf<FileScopedNamespaceDeclarationSyntax>().Name.ToString();
-
-        internal static IEnumerable<string> GetUsings(IGrouping<string, TypeDeclarationSyntax> group)
+        internal static string GetNamespace(IGrouping<string, MemberDeclarationSyntax> group)
             => group.First()
-                 .FirstAncestorOrSelf<CompilationUnitSyntax>()
-                 .DescendantNodesAndSelf()
-                 .OfType<UsingDirectiveSyntax>()
-                 .Select(x => x.Name.ToString());
+                    .FirstAncestorOrSelf<NamespaceDeclarationSyntax>()?.Name?.ToString()
+            ?? group.First()
+                    .FirstAncestorOrSelf<FileScopedNamespaceDeclarationSyntax>().Name
+                    .ToString();
 
-        internal static IEnumerable<string> GetUsings(IGrouping<string, MethodDeclarationSyntax> group)
+        internal static IEnumerable<string> GetUsings(IGrouping<string, MemberDeclarationSyntax> group)
             => group.First()
-                 .FirstAncestorOrSelf<CompilationUnitSyntax>()
-                 .DescendantNodesAndSelf()
-                 .OfType<UsingDirectiveSyntax>()
-                 .Select(x => x.Name.ToString());
-
-        internal static string GetClassName(
-            IGrouping<string, TypeDeclarationSyntax> group,
-            GeneratorAttributeType generatorType,
-            bool isDesignPatternPostfix = false,
-            bool isMainAttributeOnInterface = false)
-        {
-            string className = isDesignPatternPostfix ? group.Key + generatorType.ToString() : group.Key;
-
-            if (isMainAttributeOnInterface)
-                className = className.Substring(1);
-
-            return className;
-        }
+                    .FirstAncestorOrSelf<CompilationUnitSyntax>()
+                    .DescendantNodesAndSelf()
+                    .OfType<UsingDirectiveSyntax>()
+                    .Select(x => x.Name.ToString());
 
         internal static string GetClassName(IGrouping<string, ClassDeclarationSyntax> group) 
             => group.Key;
@@ -57,7 +35,7 @@ namespace DesignPatternCodeGenerator.Base.Generators
              => group.Key;
 
         internal static string GetClassName(IGrouping<string, InterfaceDeclarationSyntax> group)
-             => group.Key.Substring(1);
+             => group.Key.StartsWith("I") ? group.Key.Substring(1) : group.Key;
 
         internal static string GetClassName(IGrouping<string, ClassDeclarationSyntax> group, GeneratorAttributeType generatorType)
             => group.Key + generatorType.ToString();
@@ -65,15 +43,14 @@ namespace DesignPatternCodeGenerator.Base.Generators
         internal static string GetClassName(IGrouping<string, InterfaceDeclarationSyntax> group, GeneratorAttributeType generatorType)
             => group.Key.Substring(1) + generatorType.ToString();
 
-        internal static string GetInterfaceName(IGrouping<string, TypeDeclarationSyntax> group, GeneratorAttributeType generatorType)
-        { 
-            var baseInterfaceName = group.Key + generatorType.ToString();
-
-            return baseInterfaceName.StartsWith("I") ? baseInterfaceName : baseInterfaceName.Insert(0, "I");
-        }
-
-        internal static string GetInterfaceName(IGrouping<string, TypeDeclarationSyntax> group)
+        internal static string GetInterfaceName(IGrouping<string, InterfaceDeclarationSyntax> group)
             => group.Key.StartsWith("I") ? group.Key : group.Key.Insert(0, "I");
+
+        internal static string GetInterfaceName(IGrouping<string, InterfaceDeclarationSyntax> group, GeneratorAttributeType generatorType)
+            => (group.Key.StartsWith("I") ? group.Key : group.Key.Insert(0, "I")) + generatorType.ToString();
+
+        internal static string GetInterfaceName(IGrouping<string, ClassDeclarationSyntax> group, GeneratorAttributeType generatorType)
+            => (group.Key.StartsWith("I") ? group.Key : group.Key.Insert(0, "I")) + generatorType.ToString();
 
     }
 
