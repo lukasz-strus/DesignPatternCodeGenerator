b24c967ecbc4d1cb2279bdc69b706ddc9b440d53
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2022-12-11 15:26:21 +0100

    Add conteiner ioc generator file

---
+++ DesignPatternCodeGenerator/ContainerIOC/ContainerGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternCodeGenerator.ContainerIOC
+{
+    [Generator]
+    public class ContainerGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+    }
+}
